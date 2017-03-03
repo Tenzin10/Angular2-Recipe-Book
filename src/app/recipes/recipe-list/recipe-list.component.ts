1a1327eac8a4bbb8ae6702b834100fc8d0866b55
Tenzin10 <tenzin.dorjee.1534@gmail.com>
2017-03-03 18:23:25 +0530

    added shared folder and within it created a Ingredient class. //modified recipe class in src/app/recipes/recipe.ts to accomodate ingredient modified src/app/recipes/recipe.ts modified src/app/recipes/recipe-list/recipe-list.component.ts
    modified src/app/shopping-list/shopping-list.component.html
    modified:   src/app/shopping-list/shopping-list.component.ts

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,9 +7,12 @@ import { Recipe } from '../recipe';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-	recipes: Recipe[] = [];
+	recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Very tasty', 'http://images.derberater.de/files/imagecache/456xXXX_berater/berater/slides/WienerSchnitzel.jpg', []),
+    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
+  ];
 	@Output() recipeSelected = new EventEmitter<Recipe>();
-	recipe = new Recipe('Dummy','Dummy','http://thumbs3.ebaystatic.com/d/l225/m/mwJElNFGmrIXo8ucXNt1M4Q.jpg')
+	
   constructor() { }
 
   ngOnInit() {
